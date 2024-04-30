8008d6fabe05e95072d21758a24522c315238eb9
takas <163450600+Manotakas@users.noreply.github.com>
2024-04-30 13:55:18 -0300

    Update atividade1.js

--- atividade1/atividade1.js
+++ atividade1/atividade1.js
@@ -1,1 +1,9 @@
-
+//Faça um algoritimo em js com laço de 
+//repetiçao For de 1 ate 100, separes os numeros pares ate os numeros impares.
+for(var i=0; i<=100;i++){
+if(i%2==0){
+console.log(i,"= par")
+}else{
+console.log(i,"= impar")
+};
+}
